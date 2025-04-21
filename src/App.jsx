98c9a18b32d2ab452d9b46b4c9de1f93bbf9296d
AmanYadav7015 <7799amanyadav7799@gmail.com>
2025-04-22 02:29:35 +0530

    Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Login from "./components/loginPage"
 import SignUp from "./components/signUp"
 import HomePage from "./components/homePage"
+import News from "./components/News"
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom"
 
 function App() {
   
 
   return (
+    <>
   <Router>
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/news" element={<News/>}></Route>
+ 
     </Routes>
   </Router>
+  </>
   )
 }
 
