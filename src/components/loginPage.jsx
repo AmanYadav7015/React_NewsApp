60f55cf8b311e80b591b784aa7025083a08d47f0
AmanYadav7015 <7799amanyadav7799@gmail.com>
2025-04-22 18:45:07 +0530

    login page firebase done

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -5,12 +5,15 @@ import GoogleSvg from "../assets/icons8-google.svg";
 import { FaEye } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 
 
 const Login = () => {
   const [ showPassword, setShowPassword ] = useState(false);
   const navigate=useNavigate();
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
 
   return (
     <div className="login-main">
@@ -26,9 +29,13 @@ const Login = () => {
             <h2>Welcome back!</h2>
             <p>Please enter your details</p>
             <form>
-              <input type="email" placeholder="Email" />
+              <input type="email" placeholder="Email" onChange={(e)=>{
+                setEmail(e.target.value);
+              }}/>
               <div className="pass-input-div">
-                <input type={showPassword ? "text" : "password"} placeholder="Password" />
+                <input type={showPassword ? "text" : "password"} placeholder="Password"  onChange={(e)=>{
+                  setPassword(e.target.value)
+                }}/>
                 {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
                 
               </div>
@@ -36,12 +43,28 @@ const Login = () => {
               <div className="login-center-options">
                 <div className="remember-div">
                 </div>
-                <a href="#" className="forgot-pass-link" >
+                <a href="#" className="forgot-pass-link" onClick={()=>{navigate("/forgetPassword")}} >
                   Forgot password?
                 </a>
               </div>
               <div className="login-center-buttons">
-                <button type="button" onClick={()=>navigate("/news")}>Log In</button>
+                <button type="button" onClick={()=>{
+                  const auth=getAuth();
+                  
+
+                  signInWithEmailAndPassword(auth, email, password)
+                  .then((userCredential) => {
+                     
+                      navigate("/news")
+                      const user = userCredential.user;
+                      // ...
+                  })
+                  .catch((error) => {
+                      const errorCode = error.code;
+                      const errorMessage = error.message;
+                      alert(errorMessage);
+                  });
+                }}>Log In</button>
                 <button type="button">
                   <img src={GoogleSvg} alt="" />
                   Log In with Google
