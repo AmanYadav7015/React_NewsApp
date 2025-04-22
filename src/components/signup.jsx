1532d8c26fdf302f126b9e2a7e9319a6a2d05e46
AmanYadav7015 <7799amanyadav7799@gmail.com>
2025-04-22 18:44:30 +0530

    firebase of signup done

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Image from "../assets/image.png";
 import Logo from "../assets/logo.png";
-import GoogleSvg from "../assets/icons8-google.svg";
 import { FaEye } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../firebase";
 
 
-
-
-const SignUp= () => {
-  const [ showPassword, setShowPassword ] = useState(false);
-  const navigate=useNavigate();
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="login-main">
@@ -27,19 +28,63 @@ const SignUp= () => {
             <h2>SignUp Page</h2>
             <p>Please enter your details</p>
             <form>
-              <input type="email" placeholder="Email" />
+              <input
+                type="email"
+                placeholder="Email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
               <div className="pass-input-div">
-                <input type={showPassword ? "text" : "password"} placeholder="Password" />
-                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
-                
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                {showPassword ? (
+                  <FaEyeSlash
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  />
+                ) : (
+                  <FaEye
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  />
+                )}
               </div>
 
               <div className="login-center-options">
-                <div className="remember-div">
-                </div>
+                <div className="remember-div"></div>
               </div>
               <div className="login-center-buttons">
-                <button type="button">Sign Up</button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    
+                    console.log(email);
+                    console.log(password);
+                    createUserWithEmailAndPassword(auth, email, password)
+                      .then((userCredential) => {
+                        // Signed up
+                        navigate("/login");
+                        const user = userCredential.user;
+                        // ...
+                      })
+                      .catch((error) => {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        alert(errorMessage);
+                        // ..
+                      });
+                  }}
+                >
+                  Sign Up
+                </button>
               </div>
             </form>
           </div>
